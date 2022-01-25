61e90fcf62140cf0a00af16c311018fe0b87c858
John Marin <johmarin@microsoft.com>
2022-01-25 01:21:12 -0800

    Update

--- .github/actions/dhc-poc/fetchresults.js
+++ .github/actions/dhc-poc/fetchresults.js
@@ -2,7 +2,7 @@ const request = require("request-promise");
 
 const options = {
   method: "GET",
-  uri: "https://api.applicationinsights.io/v1/apps/724db0d6-7ec8-49b0-a359-456d972bde00/events/availabilityResults",
+  uri: "https://api.applicationinsights.io/v1/apps/"+core.getInput("application-id")+"/events/availabilityResults",
   headers: {
     Accept: "application/json",
     "x-api-key": core.getInput("api-key")
