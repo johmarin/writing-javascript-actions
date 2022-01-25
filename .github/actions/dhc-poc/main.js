888eb3416ac11df6680f6d660504410e415189a8
John Marin <johmarin@microsoft.com>
2022-01-25 03:03:01 -0800

    Add filtering

--- .github/actions/dhc-poc/main.js
+++ .github/actions/dhc-poc/main.js
@@ -5,12 +5,18 @@ const listWebtests = require("./listWebtests");
 async function run() {
   //const webTests = await listWebtests();
   //console.log(webTests);
+  var monitoredWebtests = new Set();
+  monitoredWebtests.add("Synthetic AIWebTests: ReleaseCompleted;West US 2")
+  const timeout = 20 * 1000
   const startTime = new Date().getTime()
   console.log("Start UTC Time: " + new Date().getTime())
-  while (new Date().getTime() - startTime < 20000) {
+  while (new Date().getTime() - startTime < timeout) {
     console.log("UTC Time: " + new Date().getTime())
     const results = await fetchResults();
     results.forEach(element => {
+      nameLocation = element.availabilityResult.name + ";" + element.availabilityResult.location
+      if (!monitoredWebtests.has(nameLocation))
+        break
       console.log(element.availabilityResult.name)
       console.log(element.availabilityResult.location)
       console.log(element.timestamp);
