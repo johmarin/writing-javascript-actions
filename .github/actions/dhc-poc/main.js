5c4e6964366279abcdfc3d03d67684f6db79c265
John Marin <johmarin@microsoft.com>
2022-01-25 02:15:24 -0800

    Add listWebtests

--- .github/actions/dhc-poc/main.js
+++ .github/actions/dhc-poc/main.js
@@ -1,15 +1,20 @@
 const core = require("@actions/core");
 const fetchResults = require("./fetchresults");
+const listWebtests = require("./listWebtests");
 
 async function run() {
+  const webTests = await listWebtests();
+  console.log(webTests);
   console.log("UTC Time: "+new Date().getTime())
   const results = await fetchResults();
   results.forEach(element => {
     console.log(element.availabilityResult.name)
+    console.log(element.availabilityResult.location)
     console.log(element.timestamp);
     console.log(element.availabilityResult.success)
   });
-  console.log("Raw JSON\n"+results);
+  console.log("Raw JSON:");
+  console.log(results)
 }
 
 run();
