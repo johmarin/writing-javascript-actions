b59bb4363ac0a52e81f2e74619f82926bc459609
John Marin <johmarin@microsoft.com>
2022-01-25 01:39:51 -0800

    Add datetime logging

--- .github/actions/dhc-poc/main.js
+++ .github/actions/dhc-poc/main.js
@@ -2,9 +2,9 @@ const core = require("@actions/core");
 const fetchResults = require("./fetchresults");
 
 async function run() {
+  console.log("UTC Time: "+new Date().getTime())
   const results = await fetchResults();
   console.log(results);
-  core.setOutput("results-output", results);
 }
 
 run();
